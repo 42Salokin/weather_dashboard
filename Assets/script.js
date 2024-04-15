e4bbf8e2a0b2531697baabb865beb3b84a349362
Salokin <nicholas.dpoul@gmail.com>
2024-04-14 20:42:03 -0400

    added pseudocode

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,7 @@
-console.log("test");
+// ToDo: make a fetch request from the weather API
+// ToDo: make a fetch request from a geolocator API
+// ToDo: take the input from the form input and put it in local storage
+// ToDo: make a function to pull the relevant geolocation info, compare it to the input, 
+// and return the lat/long coordinates of that city
+// ToDo: make a function to pull the relevant weather info for the lat/long coordinates 
+// and put them on the screen 
