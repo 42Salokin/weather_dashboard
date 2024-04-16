68abea9fddacd5937dd540547d4b6aa17a3838e5
Salokin <nicholas.dpoul@gmail.com>
2024-04-16 10:02:01 -0400

    succesfully got api fetch from openWeather, logged specific data to console

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,36 @@
+let APIKey = "6afca093eca4db8e4a356c7dbf458132";
+let city = "Orlando"
+const repoList = document.querySelector('ul');
+const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
+// const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKey}`;
 // ToDo: make a fetch request from the weather API
+
+function getAPI () {
+    fetch(queryURL)
+    //     {
+    //     method: 'GET',
+    //     headers: {
+    //     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    //     "X-RapidAPI-Key": APIKey,
+    //     }
+    // }    
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            const lat = data.coord.lat;
+            console.log(lat);
+            const lon = data.coord.lon;
+            console.log(lon); 
+            const cityName = data.name;
+            console.log(cityName); 
+            // getForecast(lat, lon, cityName);          
+        });
+    
+}
+
+getAPI();
 // ToDo: make a fetch request from a geolocator API
 // ToDo: take the input from the form input and put it in local storage
 // ToDo: make a function to pull the relevant geolocation info, compare it to the input, 
