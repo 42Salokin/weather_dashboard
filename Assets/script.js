585995abbd434f52b4b8835fbb577aca11b6979d
Salokin <nicholas.dpoul@gmail.com>
2024-04-20 18:55:12 -0400

    added input field and submit button, linked them to js, created event listener to take value from input field on click and send fetch request

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,24 @@
 let APIKey = "6afca093eca4db8e4a356c7dbf458132";
-let city = "Orlando"
+let cityInput = document.querySelector("#city");
+let cityBtn = document.querySelector("#cityBtn");
 const repoList = document.querySelector('ul');
-const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
 // const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKey}`;
 // ToDo: make a fetch request from the weather API
 
-function getAPI () {
+cityBtn.addEventListener('click', function (event) {
+    event.preventDefault;
+    let city = cityInput.value;
+    const sidebar = document.querySelector("#sidebar");
+    let cityHist = document.createElement("span");
+    cityHist.textContent = city;
+    sidebar.appendChild(cityHist);
+    getAPI(city);
+})
+
+
+function getAPI (city) {
+    console.log(city);
+    const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
     fetch(queryURL)
     //     {
     //     method: 'GET',
@@ -30,7 +43,6 @@ function getAPI () {
     
 }
 
-getAPI();
 // ToDo: make a fetch request from a geolocator API
 // ToDo: take the input from the form input and put it in local storage
 // ToDo: make a function to pull the relevant geolocation info, compare it to the input, 
